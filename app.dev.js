bd682aff9f3cc72a2a09d63d7b91a9073dd49ad5
Jonas Haas <jonas.haas@provitex.de>
2021-04-06 01:02:00 +0200

    [TASK] bugfix last comit

--- app.dev.js
+++ app.dev.js
@@ -1,16 +1,16 @@
 "use strict";
-$(document).ready(function () {
-    if ($("#switch").prop("checked")) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
-});
 
+$(document).ready(function () {
+  if ($("#switch").prop("checked")) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+});
 $("#switch").on("click", function () {
-    if ($("#switch").prop("checked")) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
+  if ($("#switch").prop("checked")) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
 });
--- app/app.js
+++ app/app.js
@@ -1,3 +1,11 @@
+$(document).ready( () => {
+    if ($("#switch").prop("checked")) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+});
+
 $("#switch").on("click", () => {
     if($("#switch").prop("checked")) {
         document.documentElement.setAttribute('data-theme', 'dark');
