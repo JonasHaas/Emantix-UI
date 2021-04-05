c8c29a706fbec03e4c89ffd51ecc5cd288b3c8c1
Jonas Haas <jonas.haas@provitex.de>
2021-04-06 00:26:05 +0200

    [BUGFIX] add right file

--- app.dev.js
+++ app.dev.js
@@ -1,1 +1,9 @@
 "use strict";
+
+$("#switch").on("click", function () {
+  if ($("#switch").prop("checked")) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+});
